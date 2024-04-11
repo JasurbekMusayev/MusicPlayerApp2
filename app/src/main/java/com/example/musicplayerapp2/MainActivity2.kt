fede787612e6b62494fb7853fbfec7dfacbd5232
SuhrobDev <suhrobqayumov4@gmail.com>
2024-04-11 17:10:24 +0500

    merged

--- app/src/main/java/com/example/musicplayerapp2/MainActivity2.kt
+++ app/src/main/java/com/example/musicplayerapp2/MainActivity2.kt
@@ -1,15 +1,7 @@
 package com.example.musicplayerapp2
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -17,25 +9,5 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_screen)
 
-        // Qo'riqchi obyekti
-        val bottomSheet = findViewById<LinearLayout>(R.id.bottom_sheet)
-
-        // BottomSheetBehavior
-        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-
-        // Qo'riqchi xaritasi
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-
-        // Qo'riqchi xaritasi harakatini boshqarish
-        bottomSheetBehavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                // Qo'riqchi xaritasi holatini boshqarish
-            }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                // Qo'riqchi xaritasi chiziqli harakati
-            }
-        })
     }
 }
